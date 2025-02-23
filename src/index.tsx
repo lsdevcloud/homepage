db5f4c3a4f51231d8e8d44f0108d0ec1cf346343
lsdevcloud <info@lsdevcloud.com>
2025-02-23 18:41:59 +0100

    Update main file

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,32 @@
+// solid 
 import { render } from 'solid-js/web';
 import { Router, Routes, Route } from "@solidjs/router";
-import App from './App';
+
+// pages
+import App from './pages/main.tsx';
+import NotFound from './pages/notfound.tsx';
+import Imprint from './pages/imprint.tsx';
+
+// components
 import Header from './components/header.tsx';
 import Footer from './components/footer.tsx';
+
+// index style sheet
 import './index.css';
 
+// page root
 const root = document.getElementById('root');
 if (!root) throw new Error("Root div not found!");
 
 render(() => (
     <>
-    <Header/>
-    <Router>
-            <Route path="/" component=<App/>/>
-    </Router>
-    <Footer/>
+        <Header/>
+            <Router>
+                <Route path="*" component=<NotFound />/>
+                <Route path="/" component=<App />/>
+                <Route path="/imprint" component=<Imprint />/>
+            </Router>
+        <Footer/>
     </>
 ), root);
 
