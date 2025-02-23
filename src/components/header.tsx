5c86cf4cf658f6f9eb122373ae1cd3ffc26ab103
lsdevcloud <info@lsdevcloud.com>
2025-02-23 18:41:19 +0100

    Update header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,10 +2,22 @@ import './header.css'
 
 export default function Header() {
 
+    function randomImage()
+    {
+
+        const random = Math.round((Math.random() * 5) + 1);
+        console.log(random);
+
+    }
+
 	return (
 		<div class='header'>
 			<div class='header-title'>
-			<h2>== LSDevCloud.com ==</h2>
+			    <h2>
+                    <a href='/'>
+                        <img src={`/hdrimg-${randomImage()}`} />
+                    </a>
+                </h2>
 			</div>
 		</div>
 	)
